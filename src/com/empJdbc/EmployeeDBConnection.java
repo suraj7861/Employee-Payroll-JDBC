add8341bdcb19ac768935701e799a3c170b0a081
unknown <khomanesuraj8@gmail.com>
2021-11-20 21:50:32 +0530

    data base connection is successful

--- src/com/empJdbc/EmployeeDBConnection.java
+++ src/com/empJdbc/EmployeeDBConnection.java
@@ -1,10 +1,43 @@
 package com.empJdbc;
 
-public class EmployeeDBConnection {
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
 
+public class EmployeeDBConnection {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee JDBC program");
 
+		String jdbcURL = "jdbc:mysql://localhost:3306/employee_payroll?useSSL=false";
+		String userName = "root";
+		String password = "Suraj@7861";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver is loaded...");		
+		}catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		checkDriver();
+		
+		Connection connection;
+		try {
+			connection = DriverManager.getConnection(jdbcURL, userName, password);
+			System.out.println("Connection is successful..!!! " + connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
+	//checking the driver is loaded or not
+	public static void checkDriver() {
+
+		Enumeration<Driver> drivers = DriverManager.getDrivers();
+		while (drivers.hasMoreElements()) {
+			Driver driver = drivers.nextElement();
+			System.out.println("Driver Name : " + driver);
+		}
+	}
 }
+
